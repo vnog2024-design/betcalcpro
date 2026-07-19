9840b7e28d0eb3eda0a7f3d13ef1bf98d74b573c
Z User <z@container>
2026-07-19 13:18:14 +0000

    fix: correct Fragment closing tag causing build failure

--- src/components/content/articles-list.tsx
+++ src/components/content/articles-list.tsx
@@ -171,7 +171,7 @@ export function ArticlesList() {
                   </CardContent>
                 </Card>
               </Link>
-            </>
+            </Fragment>
           )
         })}
       </div>
